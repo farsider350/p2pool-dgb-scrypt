8598a4972be2eab5809bf1964deb19131fab9fb3
ilsawa <ils2000@mail.ru>
2017-07-29 18:48:53 +0600

    first commit

---
+++ p2pool/networks/digibyte.py
@@ -0,0 +1,19 @@
+from p2pool.bitcoin import networks
+
+PARENT=networks.nets['digibyte']
+SHARE_PERIOD=15
+CHAIN_LENGTH=24*60*60//10
+REAL_CHAIN_LENGTH=24*60*60//10
+TARGET_LOOKBEHIND=50
+SPREAD=30
+IDENTIFIER='1bfe01eff5ba4e37'.decode('hex')
+PREFIX='1bfe01eff652e4b8'.decode('hex')
+P2P_PORT=5024
+MIN_TARGET=0
+MAX_TARGET=2**256//2**20 - 1
+PERSIST=True
+WORKER_PORT=5025
+BOOTSTRAP_ADDRS='crypto.office-on-the.net p2p-spb.xyz'.split(' ')
+ANNOUNCE_CHANNEL='#p2pool-alt'
+VERSION_CHECK=lambda v: True
+VERSION_WARNING=lambda v: 'Upgrade DigiByte >=6.14.2!' if v < 70015 else None

9dfd97602f9ee3cb3f85aaf9df2e213e4aac290f
ilsawa <ils2000@mail.ru>
2018-03-23 18:29:28 +0600

    add segwit

--- p2pool/networks/digibyte.py
+++ p2pool/networks/digibyte.py
@@ -14,6 +14,12 @@ MAX_TARGET=2**256//2**20 - 1
 PERSIST=True
 WORKER_PORT=5025
 BOOTSTRAP_ADDRS='crypto.office-on-the.net p2p-spb.xyz 188.40.110.58 siberia.mine.nu 80.211.171.166'.split(' ')
-ANNOUNCE_CHANNEL='#p2pool-alt'
-VERSION_CHECK=lambda v: True
-VERSION_WARNING=lambda v: 'Upgrade DigiByte >=6.14.2!' if v < 70015 else None
+ANNOUNCE_CHANNEL='#p2pool'
+VERSION_CHECK = lambda v: None if 6160200 <= v else 'DigiByte version too old. Upgrade to 6.16.2 or newer!'
+VERSION_WARNING = lambda v: None
+SOFTFORKS_REQUIRED = set(['nversionbips', 'csv', 'segwit'])
+MINIMUM_PROTOCOL_VERSION = 1600
+NEW_MINIMUM_PROTOCOL_VERSION = 1700
+SEGWIT_ACTIVATION_VERSION = 17
+BLOCK_MAX_SIZE = 1000000
+BLOCK_MAX_WEIGHT = 4000000
